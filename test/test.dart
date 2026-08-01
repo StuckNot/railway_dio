8de797cd0914327319d60bdec500045a908933cf
Sunpreet <sunpreet.singh@atomise.com>
2026-08-01 17:41:12 +0530

    dummy test added

--- test/test.dart
+++ test/test.dart
@@ -2,6 +2,8 @@ import 'package:test/test.dart';
 
 void main(){
   group('dummy test',(){
-    markTestSkipped('Condition met, bypassing remaining checks.');
+    test('dummy ', (){
+      markTestSkipped('Condition met, bypassing remaining checks.');
+    });
   });
 }
