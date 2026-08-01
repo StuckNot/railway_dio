0c1ae2f33a518a77ef3663cc4d47cf9e567e5131
Sunpreet <sunpreet.singh@atomise.com>
2026-08-01 18:40:13 +0530

    client factory test cases complete

--- test/client/dio_client_factory_test.dart
+++ test/client/dio_client_factory_test.dart
@@ -1,5 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:railway_dio/railway_dio.dart';
 import 'package:test/test.dart';
+
+class HttpClientAdapterMock extends Mock implements HttpClientAdapter {}
+class MockInterceptor extends Mock implements Interceptor {}
 
 void main() {
   const url = 'https://api.example.com';
@@ -20,7 +25,33 @@ void main() {
     });
     test('default connectionTime should be 15 secs', () {
       final result = buildDioClient(baseUrl: url);
-      expect(result.options.connectTimeout,  const Duration(seconds: 15));
+      expect(result.options.connectTimeout, const Duration(seconds: 15));
+    });
+    test('default receiveTimeout should be 15 secs', () {
+      final result = buildDioClient(baseUrl: url);
+      expect(result.options.receiveTimeout, const Duration(seconds: 15));
+    });
+
+    test('should use default adapter if none is provided', () {
+      final result = buildDioClient(baseUrl: url);
+      expect(result.httpClientAdapter, isNotNull);
+    });
+
+    test('should use provided adapter if provided', () {
+      final adapter = HttpClientAdapterMock();
+      final result = buildDioClient(baseUrl: url, adapter: adapter);
+      expect(result.httpClientAdapter, adapter);
+    });
+
+    test('should use default interceptor if none is provided', () {
+      final result = buildDioClient(baseUrl: url);
+      expect(result.interceptors, isNotEmpty);
+    });
+
+    test('should use provided interceptor if provided', () {
+      final interceptor = MockInterceptor();
+      final result = buildDioClient(baseUrl: url, interceptors: [interceptor]);
+      expect(result.interceptors, contains(interceptor));
     });
   });
 }
